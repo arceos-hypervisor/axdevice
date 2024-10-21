8b0661d50b878f6069568e45b0c100dec83a7271
luodeb <luodeb@outlook.com>
2024-10-21 05:55:30 +0000

    Deploying to gh-pages from @ arceos-hypervisor/axdevice@51bf078bb980af35c4dc4ef08e4077ade1073453 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["axdevice",["",[],["config.rs","device.rs","lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[55]}
---
+++ trait.impl/core/marker/trait.Freeze.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["axdevice",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"axdevice/struct.AxVmDeviceConfig.html\" title=\"struct axdevice::AxVmDeviceConfig\">AxVmDeviceConfig</a>",1,["axdevice::config::AxVmDeviceConfig"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"axdevice/struct.AxVmDevices.html\" title=\"struct axdevice::AxVmDevices\">AxVmDevices</a>",1,["axdevice::device::AxVmDevices"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[646]}
