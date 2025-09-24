eb10d07c846aa74a1b0171bc61fd28f1429fca3e
hky1999 <976929993@qq.com>
2025-09-24 22:03:27 +0800

    [feat] add DUMMY pci_host in AxVmDevices to mock PIO operations

--- src/device.rs
+++ src/device.rs
@@ -3,24 +3,34 @@ use crate::AxVmDeviceConfig;
 use alloc::sync::Arc;
 use alloc::vec::Vec;
 
+use spin::Mutex;
+
 use axaddrspace::GuestPhysAddr;
 use axdevice_base::BaseDeviceOps;
 use axerrno::AxResult;
+use axvirtpci::{PciHost, PioOps};
 use axvmconfig::EmulatedDeviceConfig;
 
 /// represent A vm own devices
 pub struct AxVmDevices {
     /// emu devices
     emu_devices: Vec<Arc<dyn BaseDeviceOps>>,
+    /// A Dummy pci host only for guest VM.
+    pci_host: Option<Arc<Mutex<PciHost>>>,
     // TODO passthrough devices or other type devices ...
 }
 
 /// The implemention for AxVmDevices
 impl AxVmDevices {
     /// According AxVmDeviceConfig to init the AxVmDevices
-    pub fn new(config: AxVmDeviceConfig) -> Self {
+    pub fn new(config: AxVmDeviceConfig, is_host: bool) -> Self {
         let mut this = Self {
             emu_devices: Vec::new(),
+            pci_host: if !is_host {
+                Some(Arc::new(Mutex::new(PciHost::new())))
+            } else {
+                None
+            },
         };
 
         Self::init(&mut this, &config.emu_configs);
@@ -89,5 +99,39 @@ impl AxVmDevices {
             "emu_handler: no emul handler for data abort ipa {:#x}",
             addr
         );
+    }
+
+    /// Handle the PIO read by port and access_size, return the value of the guest want to read.
+    /// TODO: merge it with new version of [`axdevice_base::BasePortDeviceOps`](https://github.com/arceos-hypervisor/axdevice_base/blob/35da6c7b5a29d01410df6ca31300bbfc50eb4603/src/lib.rs#L80C11-L80C29)
+    pub fn handle_pio_read(&self, port: u16, access_size: u8) -> AxResult<u32> {
+        match &self.pci_host {
+            Some(pci_host) => {
+                let mut pci_host = pci_host.lock();
+                if pci_host.port_range().contains(&port) {
+                    return pci_host.read(port, access_size);
+                } else {
+                    return Err(axerrno::AxError::InvalidInput);
+                }
+            }
+            None => Err(axerrno::AxError::InvalidInput),
+        }
+    }
+
+    /// Handle the PIO write by port, access_size and the value need to write, call specific device to write the value.
+    ///
+    /// Currently, only the pci host device support PIO operation.
+    // TODO: merge it with new version of [`axdevice_base::BasePortDeviceOps`](https://github.com/arceos-hypervisor/axdevice_base/blob/35da6c7b5a29d01410df6ca31300bbfc50eb4603/src/lib.rs#L80C11-L80C29)
+    pub fn handle_pio_write(&self, port: u16, access_size: u8, value: u32) -> AxResult {
+        match &self.pci_host {
+            Some(pci_host) => {
+                let mut pci_host = pci_host.lock();
+                if pci_host.port_range().contains(&port) {
+                    return pci_host.write(port, access_size, value);
+                } else {
+                    return Err(axerrno::AxError::InvalidInput);
+                }
+            }
+            None => Err(axerrno::AxError::InvalidInput),
+        }
     }
 }
